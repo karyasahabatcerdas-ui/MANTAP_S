0aceece17f9ed5b38aa0873ea27cc86d1b5e33ba
nofadin <karyasahabatcerdas@gmail.com>
2026-03-13 05:11:14 +0700

    inline block dan header 0.8

--- loginScript.js
+++ loginScript.js
@@ -186,7 +186,7 @@ async function checkSessionAndLogin() {
 
       const adminArea = document.getElementById('adminMenuArea');
       if (adminArea) {
-        adminArea.style.display = (userRole === 'admin') ? 'inline-blok' : 'none';
+        adminArea.style.display = (userRole === 'admin') ? 'inline-block' : 'none';
       }
 
       // 3. Jalankan Sinkronisasi Data (Wuzzz!)
